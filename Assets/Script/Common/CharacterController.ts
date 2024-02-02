e0b4b2e9f15a16be3e6de60fbf04e70ccad78df9
junyoungp <junyoung.p@linecorp.com>
2024-02-02 10:13:03 +0900

    슈가랜드 시작 브랜치 추가.

--- Assets/Script/Common/CharacterController.ts
+++ Assets/Script/Common/CharacterController.ts
@@ -1,7 +1,6 @@
 import { LocalPlayer, SpawnInfo, ZepetoCharacter, ZepetoPlayers } from 'ZEPETO.Character.Controller';
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
 import { WorldService } from 'ZEPETO.World';
-import CharacterPlayer from './CharacterPlayer';
 
 export default class CharacterController extends ZepetoScriptBehaviour {
 
@@ -10,10 +9,6 @@ export default class CharacterController extends ZepetoScriptBehaviour {
         ZepetoPlayers.instance.CreatePlayerWithUserId(WorldService.userId, new SpawnInfo(), true);            
         ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {           
             const player: LocalPlayer = ZepetoPlayers.instance.LocalPlayer;      
-            
-            // 캐릭터에 내가 제어할 수 있는 스크립트 추가.
-            var zepetoCharacter: ZepetoCharacter = player.zepetoPlayer.character;
-            zepetoCharacter.gameObject.AddComponent<CharacterPlayer>();
         });   
     }
 
