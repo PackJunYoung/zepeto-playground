b34f31d70ccd0c44458dc4932ff6c5581d74a8ca
junyoungp <junyoung.p@linecorp.com>
2024-03-08 15:38:02 +0900

    ThemePark start

--- Assets/Script/Common/CharacterController.ts
+++ Assets/Script/Common/CharacterController.ts
@@ -1,10 +1,7 @@
 import { LocalPlayer, SpawnInfo, ZepetoCharacter, ZepetoPlayers } from 'ZEPETO.Character.Controller';
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
-import { OfficialContentType, WorldService, ZepetoWorldContent } from 'ZEPETO.World';
+import { WorldService } from 'ZEPETO.World';
 import CharacterPlayer from './CharacterPlayer';
-import { GameObject } from 'UnityEngine';
-import ScreenShotController from '../PartyRoom/ScreenShotController';
-import UiController from '../PartyRoom/UiController';
 
 export default class CharacterController extends ZepetoScriptBehaviour {
 
@@ -13,23 +10,11 @@ export default class CharacterController extends ZepetoScriptBehaviour {
         ZepetoPlayers.instance.CreatePlayerWithUserId(WorldService.userId, new SpawnInfo(), true);            
         ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {           
             const player: LocalPlayer = ZepetoPlayers.instance.LocalPlayer;      
-            ScreenShotController.instance.SetCamera(player.zepetoCamera.camera);
 
             // 캐릭터에 내가 제어할 수 있는 스크립트 추가.
             var zepetoCharacter: ZepetoCharacter = player.zepetoPlayer.character;
             var characterPlayer = zepetoCharacter.gameObject.AddComponent<CharacterPlayer>();
-
-            // 제스처 설정하기.
-            UiController.instance.SetCharacterPlayer(characterPlayer);
-            this.ContentRequest(characterPlayer);
         });   
-    }
-
-    private ContentRequest(characterPlayer: CharacterPlayer) {
-        ZepetoWorldContent.RequestOfficialContentList(OfficialContentType.All, contents => {
-            contents.forEach(content => console.log(`${content.Id} : ${content.Title}`));
-            characterPlayer.SetContents(contents);
-        });
     }
 
 }
